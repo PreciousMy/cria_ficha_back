2a53a595a9dd81b8ae6a5948ae417f96d627d012
PreciousMy <alvaromaiachaves@gmail.com>
2025-06-10 15:31:06 -0300

    Criação do serializers "completa"

--- src/fichas/serializers.py
+++ src/fichas/serializers.py
@@ -1,1 +1,47 @@
 from rest_framework import serializers
+from .models import Usuario, Ficha, Pericia_Teste, Arma_Ataque, Arma, Magia, Lista_Magia, Armadura, Classe_Armadura
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ("nome", "email"," foto_perfil" )  # Eu acho que deve olcutar a senha
+
+class FichaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ficha
+        fields = '__all__'
+
+class Pericia_TesteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pericia_Teste
+        fields = '__all__'
+
+class Arma_AtaqueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Arma_Ataque
+        fields = '__all__'
+
+class ArmaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Arma
+        fields = '__all__'
+
+class MagiaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Magia
+        fields = '__all__'
+
+class Lista_MagiaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lista_Magia
+        fields = '__all__'
+
+class ArmaduraSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Armadura
+        fields = '__all__'
+
+class Classe_ArmaduraSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Classe_Armadura
+        fields = '__all__'
--- src/fichas/views.py
+++ src/fichas/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 from rest_framework import filters, viewsets
+from .models import Usuario, Ficha, Pericia_Teste, Arma_Ataque, Arma, Magia, Lista_Magia, Armadura, Classe_Armadura
+from .serializers import UsuarioSerializer,Ficha, Pericia_TesteSerializer, Arma_AtaqueSerializer, ArmaSerializer,MagiaSerializer, Lista_MagiaSerializer, ArmaduraSerializer, Classe_ArmaduraSerializer
+
 # Create your views here.
